6a7e0d66303e9255019d6a9ec99a2fc90adf9dbb
JamesVanWaza <jgacuca@gmail.com>
2023-01-02 16:49:53 -0500

    Finish the updateQuery

--- src/js/FriendlyEats/FriendlyEats.View.js
+++ src/js/FriendlyEats/FriendlyEats.View.js
@@ -284,3 +284,33 @@ FriendlyEats.prototype.initFilterDialog = function() {
         });
     });
 }
+
+FriendlyEats.prototype.updateQuery = function(filters) {
+    var query_description = '';
+
+    if (filters.category !== '') {
+        query_description += filters.category + ' places';
+    } else {
+        query_description += 'any restaurant';
+    }
+
+    if (filters.city !== '') {
+        query_description += ' in ' + filters.city;
+    } else {
+        query_description += ' located anywhere';
+    }
+
+    if (filters.price !== '') {
+        query_description += ' with a price of ' + filters.price;
+    } else {
+        query_description += 'with any price';
+    }
+
+    if (filters.sort === 'Rating') {
+        query_description += ' sorted by Rating';
+    } else if (filters.sort === 'Reviews') {
+        query_description += ' sorted by # of reviews';
+    }
+
+    this.viewList(filters, query_description);
+}
